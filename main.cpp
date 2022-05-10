e6354a390899460d357be423d2aa1c3ade9ec1e2
Kir1K2313 <100360486+Kir1K2313@users.noreply.github.com>
2022-05-10 18:55:41 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 
-
 #include "TXLib.h"
+#include <fstream>
+using namespace std;
 
 bool click_answer(int x)
 {
@@ -19,13 +20,19 @@ struct Form
   string text_answer2;
   string text_answer3;
 };
-
 void draw_answer(int x, string text)
 {
   txSetFillColor(TX_BLACK);
   Win32::RoundRect(txDC(), x, 350, x+200, 450, 20, 20 );
   txDrawText (x, 350, x+200, 450, text.c_str());
 
+}
+string getPart(string str, int pos2)
+{
+    int pos1 = &pos2 + 1;
+    &pos2 = str.find(",", pos1);
+    string Part = str.substr(pos1,*pos2 - pos1);
+    return Part;
 }
 
 int main()
@@ -34,19 +41,32 @@ int main()
     txTextCursor (false);
 
     int n_question = 1;
-    int kol_question = 5;
     int result = 0;
 
  Form form;
 
- Form form_list[5];
-  form_list[0]={"1) Сколько существует на данный момент элементов?", 2,"1) 6","2) 7","3) 8"};
-  form_list[1]={"2) В каком мире происходят действия?", 1,"1) Тейват","2) Селестия","3) Ли юэ"};
-  form_list[2]={"3) Как зовут нашего помощника?", 2,"1) Итер","2) Паймон","3) Люмин"};
-  form_list[3]={"4) Как называют защитника региона?", 3,"1) Боги истоков","2) Фатуи","3) Архонт"};
-  form_list[4]={"5) Какой предмет помогает овладеть элементом?", 1,"1) Глаз бога","2) Оружие","3) Артефакт"};
+ Form form_list[10];
 
- while(n_question<=kol_question)
+    string str;
+    int n = 0;
+    ifstream file("13131.txt");
+
+    while(file.good())
+    {
+    getline(file, str);
+    int pos2 = -1;
+
+    form_list[n].text_question = getPart( str,&pos2);
+    form_list[n].right_answer = atoi(getPart( str,&pos2).c_str())
+    form_list[n].text_answer1 = getPart( str,&pos2);
+    form_list[n].text_answer2 = getPart( str,&pos2);
+    form_list[n].text_answer3 = getPart( str,&pos2);
+    n++;
+    }
+
+ file.close();
+
+ while(n_question<=n)
  {
     txBegin();
     txSetFillColor(TX_BLACK);
@@ -57,14 +77,14 @@ int main()
 
     txSetColor(TX_WHITE);
     txSetFillColor(TX_WHITE);
-    txSelectFont("Arial", 30);
+    txSelectFont("Arial", 25);
     txDrawText(50, 50 ,750, 250, form.text_question.c_str());
     draw_answer(50, form.text_answer1);
     draw_answer(300, form.text_answer2);
     draw_answer(550, form.text_answer3);
 
     char str[10];
-    sprintf(str, "Вопрос %d/%d", n_question, kol_question);
+    sprintf(str, "Вопрос %d/%d", n_question, n);
     txDrawText(50, 0 ,750, 50, str );
 
     if (click_answer(50))
@@ -103,15 +123,15 @@ int main()
     txSetFillColor(TX_BLACK);
     txClear();
      txDrawText(50, 50 ,750, 250, "Ваш результат");
-    char str[10];
-    sprintf(str, "%d", result);
-     txDrawText(50, 150 ,750, 550, str);
+    char stroka[10];
+    sprintf(stroka, "%d", result);
+     txDrawText(50, 150 ,750, 550, stroka);
 
-     if (result==5)
+     if (result==10)
      txDrawText(50, 350 ,750, 250, "Знаток" );
-     else if(3 <= result <= 5)
+     else if(5<= result <=10)
      txDrawText(50, 350 ,750, 250, "Cредний" );
-     else if(result<= 3)
+     else if(result<5)
      txDrawText(50, 350 ,750, 250, "Новичок" );
     return 0;
    }
