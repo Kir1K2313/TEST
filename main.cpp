d4d0b08d553b40f62252ff32659d968b315bc4c3
Kir1K2313 <100360486+Kir1K2313@users.noreply.github.com>
2022-05-19 18:45:28 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,26 +19,35 @@ struct Form
   string text_answer1;
   string text_answer2;
   string text_answer3;
+  HDC pic_fon;
+
 };
-void draw_answer(int x, string text)
+void draw_answer(int x, string text, int font)
 {
+  txSetColor(TX_WHITE);
   txSetFillColor(TX_BLACK);
+  txSelectFont("Arial", font);
   Win32::RoundRect(txDC(), x, 350, x+200, 450, 20, 20 );
   txDrawText (x, 350, x+200, 450, text.c_str());
 
 }
-string getPart(string str, int pos2)
+string getPart(string str, int *pos2)
 {
-    int pos1 = &pos2 + 1;
-    &pos2 = str.find(",", pos1);
+    int pos1 = *pos2 + 1;
+    *pos2 = str.find(",", pos1);
     string Part = str.substr(pos1,*pos2 - pos1);
     return Part;
 }
+
 
 int main()
     {
     txCreateWindow (800, 600);
     txTextCursor (false);
+
+    HDC fon1 = txLoadImage("картинки/фон.bmp");
+
+
 
     int n_question = 1;
     int result = 0;
@@ -50,6 +59,7 @@ int main()
     string str;
     int n = 0;
     ifstream file("13131.txt");
+    int font = 35;
 
     while(file.good())
     {
@@ -57,10 +67,11 @@ int main()
     int pos2 = -1;
 
     form_list[n].text_question = getPart( str,&pos2);
-    form_list[n].right_answer = atoi(getPart( str,&pos2).c_str())
+    form_list[n].right_answer = atoi(getPart( str,&pos2).c_str());
     form_list[n].text_answer1 = getPart( str,&pos2);
     form_list[n].text_answer2 = getPart( str,&pos2);
     form_list[n].text_answer3 = getPart( str,&pos2);
+    form_list[n].pic_fon = txLoadImage(getPart(str,&pos2).c_str());
     n++;
     }
 
@@ -75,14 +86,23 @@ int main()
   form = form_list[n_question - 1];
 
 
-    txSetColor(TX_WHITE);
+    txSetColor(TX_BLACK);
     txSetFillColor(TX_WHITE);
-    txSelectFont("Arial", 25);
+    font = 35;
+    txSelectFont("Arial", font);
+    txBitBlt (txDC(), 0, 0, 800,600, form.pic_fon);
     txDrawText(50, 50 ,750, 250, form.text_question.c_str());
-    draw_answer(50, form.text_answer1);
-    draw_answer(300, form.text_answer2);
-    draw_answer(550, form.text_answer3);
 
+    if(n_question == 8)
+    {
+       font = 25;
+    }
+    draw_answer(50, form.text_answer1, font);
+    draw_answer(300, form.text_answer2, font);
+    draw_answer(550, form.text_answer3, font);
+
+    font = 35;
+    txSelectFont("Arial", font);
     char str[10];
     sprintf(str, "Вопрос %d/%d", n_question, n);
     txDrawText(50, 0 ,750, 50, str );
@@ -116,12 +136,12 @@ int main()
       }
     }
 
-
     txEnd;
     txSleep(50);
  }
     txSetFillColor(TX_BLACK);
     txClear();
+
      txDrawText(50, 50 ,750, 250, "Ваш результат");
     char stroka[10];
     sprintf(stroka, "%d", result);
@@ -133,5 +153,11 @@ int main()
      txDrawText(50, 350 ,750, 250, "Cредний" );
      else if(result<5)
      txDrawText(50, 350 ,750, 250, "Новичок" );
+
+     for(int i=0; i<=n; i++)
+     {
+        txDeleteDC(form_list[i].pic_fon);
+     }
+
     return 0;
    }
